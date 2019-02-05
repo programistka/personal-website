0a14687a3c100676605aba69ffec469745799d97
Robert Cooper <robert.cooper.qu@gmail.com>
2019-02-04 22:40:15 -0500

    RSS feed adjustments

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,34 +75,39 @@ module.exports = {
                             return allMdx.edges.map(edge => {
                                 return {
                                     ...edge.node.frontmatter,
-                                    url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                                    guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
-                                    custom_elements: [{ 'content:encoded': edge.node.rawBody }],
+                                    description: edge.node.excerpt,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ 'content:encoded': edge.node.html }],
                                 };
                             });
                         },
                         query: `
-            {
-              allMdx(
-                limit: 1000,
-                sort: {
-                  order: DESC,
-                  fields: [frontmatter___date]
-                },
-                filter: { fields: { slug: { ne: null } } }
-              ) {
-                edges {
-                  node {
-                    frontmatter {
-                      title
-                      date
-                      description
+                    {
+                      allMdx(
+                        limit: 1000,
+                        sort: {
+                          order: DESC,
+                          fields: [frontmatter___date]
+                        },
+                        filter: { fields: { slug: { ne: null } } }
+                      ) {
+                        edges {
+                          node {
+                            frontmatter {
+                              title
+                              date
+                            }
+                            fields {
+                              slug
+                            }
+                            excerpt
+                            html
+                          }
+                        }
+                      }
                     }
-                  }
-                }
-              }
-            }
-          `,
+                  `,
                         output: `rss.xml`,
                     },
                 ],
